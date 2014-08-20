8a5ab8111334461ce1c71793014303654b0269bf
smparker1201 <sarah_m_parker@brown.edu>
2014-08-20 05:54:06 -0400

    pushing remaining files

--- imageIO.py
+++ imageIO.py
@@ -1,13 +1,23 @@
 import scipy as sp
 from scipy import misc
 from PIL import Image
+import math
 import os
 
+def makeSquare(image):
+	'''For images that aren't square, this funtion will identify the closest power of 2 
+	to the smaller dimension and decide whether to crop or pad. If it needs to be padded, 
+	this is done in harris.hs'''
+ 	dim = min(sp.shape(image)[0],sp.shape(image)[1])
+	potential_crop = int(2**math.floor(math.log(dim,2)))
+	if potential_crop**2 >= 0.9*sp.shape(image)[0]*sp.shape(image)[1]:
+		return sp.misc.imresize(image,(potential_crop,potential_crop))
 
+	return image
 
 def writeSMI(filename="small_image.jpg"):
 	im = Image.open(filename)
-	im = sp.array(im.convert('L'))
+	im = makeSquare(sp.array(im.convert('L')))
 	im = im.tolist()
 	myString = '['
 	for r,row in enumerate(im):
